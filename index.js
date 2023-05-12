ce5ef8cd4161c744869f74a90ce0691a2750193a
Haoua Enikoye <ienikoye@gmail.com>
2023-05-12 12:46:11 +0100

    Update sendMail

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function sendEmail(){
 
     emailjs.send("service_9hlcjnm", "template_ct1ecuf", params).then(function (res) {
         alert("Votre message a été livré avec succés"+ res.status);
+        document.getElementById("fullName").value= '';
+        document.getElementById("email_id").value='';
+        document.getElementById("message").value='';
     })
-   
+   console.log('yo');
 }
